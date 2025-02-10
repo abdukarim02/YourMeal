6e1d228a4505ceb258a1923d14e58b9bd2a2a9f4
abdukarim <kaumijabdukarim@gmail.com>
2025-02-10 20:09:15 +0500

    123

---
+++ src/components/Product.vue
@@ -0,0 +1,152 @@
+<template>
+<section class="main">
+      <div class="main__content container">
+        <div class="categories">
+          <div class="categories__content">
+            <button class="categories__content-btn active">
+              <img src="/public/filter-1.png" alt="filter-1" class="categories__btn-img">
+              <span class="categories__btn-name">Бургеры</span>
+            </button>
+            <button class="categories__content-btn">
+              <img src="/public/filter-2.png" alt="filter-2" class="categories__btn-img">
+              <span class="categories__btn-name">Закуски</span>
+            </button>
+            <button class="categories__content-btn">
+              <img src="/public/filter-3.png" alt="filter-3" class="categories__btn-img">
+              <span class="categories__btn-name">Хот-доги</span>
+            </button>
+            <button class="categories__content-btn">
+              <img src="/public/filter-1.png" alt="filter-1" class="categories__btn-img">
+              <span class="categories__btn-name">Бургеры</span>
+            </button>
+            <button class="categories__content-btn">
+              <img src="/public/filter-2.png" alt="filter-2" class="categories__btn-img">
+              <span class="categories__btn-name">Закуски</span>
+            </button>
+            <button class="categories__content-btn">
+              <img src="/public/filter-3.png" alt="filter-3" class="categories__btn-img">
+              <span class="categories__btn-name">Хот-доги</span>
+            </button>
+            <button class="categories__content-btn">
+              <img src="/public/filter-1.png" alt="filter-1" class="categories__btn-img">
+              <span class="categories__btn-name">Бургеры</span>
+            </button>
+            <button class="categories__content-btn">
+              <img src="/public/filter-2.png" alt="filter-2" class="categories__btn-img">
+              <span class="categories__btn-name">Закуски</span>
+            </button>
+            <button class="categories__content-btn">
+              <img src="/public/filter-3.png" alt="filter-3" class="categories__btn-img">
+              <span class="categories__btn-name">Хот-доги</span>
+            </button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card__content">
+            <div class="card__content-header">
+              <h5 class="card__header-name">Корзина</h5>
+              <span class="card__header-number">4</span>
+            </div>
+            <div class="card__content-basket">
+              <div class="card__basket-body">
+                <div class="card__body-bg">
+                  <img src="/public/product-1.png" alt="">
+                </div>
+                <div class="card__body-info">
+                  <h5 class="card__info-name">Супер сырный</h5>
+                  <span class="card__info-gram">512г</span>
+                  <p class="card__info-price">245₽</p>
+                </div>
+                <button class="card__body-button">
+                  <span>-</span>
+                  <span class="card__button-number">1</span>
+                  <span>+</span>
+                </button>
+              </div>
+            </div>
+            <div class="card__content-footer">
+              <div class="card__footer-total">
+                <h5 class="card__total-name">Итого</h5>
+                <span class="card__total-price">1279₽</span>
+              </div>
+              <button class="card__footer-btn">Оформить заказ</button>
+              <div class="card__content-delivery">
+                <img src="/public/delivery.png" alt="delivery">
+                <p>Бесплатная доставка</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="product">
+          <div class="product__wrap">
+            <div class="product__wrap-flex">
+              <div class="product__flex-bg">
+                <img src="/public/product-1.png" alt="product-1" class="product__bg-img">
+              </div>
+              <div class="product__flex-info">
+                <p class="product__info-price">689₽</p>
+                <p class="product__info-name">Мясная бомба</p>
+                <p class="product__info-gram">520г</p>
+              </div>
+              <button class="product__flex-btn">Добавить</button>
+            </div>
+            <div class="product__wrap-flex">
+              <div class="product__flex-bg">
+                <img src="/public/product-1.png" alt="product-1" class="product__bg-img">
+              </div>
+              <div class="product__flex-info">
+                <p class="product__info-price">689₽</p>
+                <p class="product__info-name">Мясная бомба</p>
+                <p class="product__info-gram">520г</p>
+              </div>
+              <button class="product__flex-btn">Добавить</button>
+            </div>
+            <div class="product__wrap-flex">
+              <div class="product__flex-bg">
+                <img src="/public/product-1.png" alt="product-1" class="product__bg-img">
+              </div>
+              <div class="product__flex-info">
+                <p class="product__info-price">689₽</p>
+                <p class="product__info-name">Мясная бомба</p>
+                <p class="product__info-gram">520г</p>
+              </div>
+              <button class="product__flex-btn">Добавить</button>
+            </div>
+            <div class="product__wrap-flex">
+              <div class="product__flex-bg">
+                <img src="/public/product-1.png" alt="product-1" class="product__bg-img">
+              </div>
+              <div class="product__flex-info">
+                <p class="product__info-price">689₽</p>
+                <p class="product__info-name">Мясная бомба</p>
+                <p class="product__info-gram">520г</p>
+              </div>
+              <button class="product__flex-btn">Добавить</button>
+            </div>
+            <div class="product__wrap-flex">
+              <div class="product__flex-bg">
+                <img src="/public/product-1.png" alt="product-1" class="product__bg-img">
+              </div>
+              <div class="product__flex-info">
+                <p class="product__info-price">689₽</p>
+                <p class="product__info-name">Мясная бомба</p>
+                <p class="product__info-gram">520г</p>
+              </div>
+              <button class="product__flex-btn">Добавить</button>
+            </div>
+            <div class="product__wrap-flex">
+              <div class="product__flex-bg">
+                <img src="/public/product-1.png" alt="product-1" class="product__bg-img">
+              </div>
+              <div class="product__flex-info">
+                <p class="product__info-price">689₽</p>
+                <p class="product__info-name">Мясная бомба</p>
+                <p class="product__info-gram">520г</p>
+              </div>
+              <button class="product__flex-btn">Добавить</button>
+            </div>
+          </div>
+        </div>
+      </div>
+</section>
+</template>
